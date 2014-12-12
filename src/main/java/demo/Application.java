7b6e1b85695b0849772e55488cce0113c51b08ad
Andrew Lombardi <andrew@mysticcoders.com>
2014-12-11 16:37:55 -0800

    remove the ugly spring web thing

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -23,14 +23,6 @@ public class Application {
 
 }
 
-@RestController
-class Test {
-    @RequestMapping("/foo")
-    String hi() {
-        return "Hi";
-    }
-}
-
 @Configuration
 class WicketServletConfiguration {
 
